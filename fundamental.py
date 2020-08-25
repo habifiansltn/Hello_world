1f0530b0e69d910d667ff854b300dd939a711adf
hello_world <habifian@hotmail.com>
2020-08-25 22:02:40 +0700

    file baru

---
+++ fundamental.py
@@ -0,0 +1,136 @@
+# nama = input('nama kamu?')
+# umur = input('umur kamu?')
+# kelamin = input ('kelamin kamu?')
+# pekerjaan = input('pekerjaan kamu?')
+#
+# print(nama)
+# print(umur)
+# print(kelamin)
+# print(pekerjaan)
+
+# usiaandi = 40
+# usiabudi = 20
+#
+# print(usiaandi * usiabudi)
+# print(usiaandi / usiabudi)
+# print(usiaandi + usiabudi)
+# print(usiaandi - usiabudi);
+# print(usiaandi % usiabudi);
+# print(usiabudi ** 2);
+
+# x=int(input("x="))
+# y=int(input("y="))
+# z=int(input("z="))
+#
+# w=((x+y*z)/(x*y))**z
+#
+# print(w)
+
+import math
+# print(math.pi)
+# print(math.fabs(-4.7))
+# print(math.pow(8,2))
+# print(math.sqrt(64))
+
+# x = 'hallo dunia'
+# print(len(x))
+# print(x.index('dunia'))
+# print(x.split('o'))
+# print(x.lower())
+# print(x.upper())
+# print(x.capitalize())
+
+# singleQuotes = 'singleQuotes';
+# doubleQuotes = "double quotes";
+# combineQuotes = "wrap lot's of other quotes"
+
+# print(singleQuotes)
+# print(double quotes)
+# print(combineQuotes)
+
+# text = "i'm baron, nice to meet you"
+# print(text[1])
+# angka1 =  input('masukan angka 1 =')
+# angka2 = input('masukan angka 2 =')
+# print(int(angka1) + int(angka2))
+
+# usia = 22
+# nama = 'andi'
+#
+# print(usia + usia)
+# print(nama + ' ' + str(usia))
+
+# x=int(input("x="))
+# y=int(input("y="))
+# z=int(input("z="))
+#
+# w=((x+y*z)/(x*y))**z
+#
+# print(w)
+
+# angka = input('silahkan masukan angka berapapun :')
+# print('kuadrat dari {} = {}'.format(angka,pow(int(angka), 2)))
+
+# import math
+#
+# text = input('berapa hari?')
+# hari = int(text)
+# tahun = str(math.floor(hari/360))
+# sisa_hari = hari%360
+#
+# bulan = str(math.floor((sisa_hari/30)))
+# sisa_hari %= 30
+#
+# minggu = str(math.floor(sisa_hari/7))
+# sisa_hari %= 7
+#
+# hari = str(sisa_hari)
+#
+# statement = print(tahun + 'Tahun', bulan + "Bulan", minggu + 'Minggu', hari + 'Hari')
+# print(statement)
+
+# name = 'habi'
+# print('hello, %s!' % name)
+
+# usiatotal = 49
+# rasio = 4/10
+# x = 49/14
+# usiaandi = 4 * x
+# usiabudi = 10 * x
+# # print(int(x))
+# # print(int(usiaandi))
+# # print(int(usiabudi))
+#
+# usiaandi += 2
+# usiabudi += 2
+#
+# print("usia andi =", int(usiaandi))
+# print('usia budi =', int(usiabudi))
+
+
+# text = 'nama saya habifian sultan saya seorang mahasiswa jurusan management'
+# huruf = input('masukan huruf:')
+# print(text.count(huruf))
+
+# distance = 120
+# Va = 60
+# Vb = 40
+# time = 9
+#
+# crash = distance / (Va + Vb)
+# print(crash)
+# print('bertabrakan pada pukul :', (crash + 9))
+
+# jarak = input("jumlah jarak: ")
+# kecepatan = input("jumlah kecepatan: ")
+# sisa_waktu = int(jarak) % int(kecepatan)
+# tabrak = sisa_waktu / int(kecepatan) * 60
+# tambahan_waktu = "1 jam " + str(tabrak) + " menit "
+# print("Apabila total jarak " + str(jarak) + " dan total kecepatan " + str(kecepatan) + ", kedua mobil akan menabrak pada " + str(tambahan_waktu))
+
+
+
+
+
+
+
